a30d696012183e62e949ee8dc7b38e09da265981
Joachim Alexandre Jasmin <joachim.jasmin-dev@proton.me>
2026-06-25 22:16:09 +0200

    feat: added resolvers for tables

--- src/table/table.model.ts
+++ src/table/table.model.ts
@@ -1,10 +1,9 @@
 import { Field, Int, ObjectType } from '@nestjs/graphql';
-import { Restaurant } from '../restaurant/restaurant.model';
 
 @ObjectType()
 export class Table {
   @Field(() => Int) id!: number;
   @Field(() => Int) number!: number;
   @Field(() => Int) seats!: number;
-  @Field(() => Restaurant) restaurant!: Restaurant;
+  @Field(() => Int) restaurantId!: number;
 }
--- src/table/table.resolver.ts
+++ src/table/table.resolver.ts
@@ -1,4 +1,38 @@
-import { Resolver } from '@nestjs/graphql';
+import {
+  Args,
+  Int,
+  Parent,
+  Query,
+  ResolveField,
+  Resolver,
+} from '@nestjs/graphql';
+import { TableService } from './table.service';
+import { Table } from './table.model';
+import { Restaurant } from '../restaurant/restaurant.model';
+import { RestaurantService } from '../restaurant/restaurant.service';
 
-@Resolver()
-export class TableResolver {}
+@Resolver(() => Table)
+export class TableResolver {
+  constructor(
+    private service: TableService,
+    private restaurantService: RestaurantService,
+  ) {}
+
+  // Get all tables
+  @Query(() => [Table])
+  async tables() {
+    return this.service.findAll();
+  }
+
+  // Get single table by ID
+  @Query(() => Table)
+  async table(@Args('id', { type: () => Int }) id: number) {
+    return this.service.findOne({ id });
+  }
+
+  // Populate restaurants by table
+  @ResolveField('restaurant', () => Restaurant)
+  async restaurants(@Parent() table: Table) {
+    return this.restaurantService.findOne({ id: table.restaurantId });
+  }
+}

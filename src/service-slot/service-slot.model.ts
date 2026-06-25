506b7dcf68183b70fda216a2279861be0867141e
Joachim Alexandre Jasmin <joachim.jasmin-dev@proton.me>
2026-06-25 22:15:48 +0200

    fix: removed unused  field restaurant

--- src/service-slot/service-slot.model.ts
+++ src/service-slot/service-slot.model.ts
@@ -1,13 +1,12 @@
 import { Field, Int, ObjectType } from '@nestjs/graphql';
-import { Restaurant } from '../restaurant/restaurant.model';
 
 @ObjectType()
 export class ServiceSlot {
   @Field(() => Int) id!: number;
+  @Field(() => Int) restaurantId!: number;
   @Field(() => String) date!: string;
   @Field(() => String) period!: string;
   @Field(() => String) openingTime!: string;
   @Field(() => String) closingTime!: string;
-  @Field(() => Restaurant) restaurant!: Restaurant;
   @Field(() => Int) capacity!: number;
 }

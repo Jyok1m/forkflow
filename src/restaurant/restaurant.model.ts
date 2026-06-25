1bf80fb2bd4d7f2f0f4389701fde8b2ac0536d81
Joachim Alexandre Jasmin <joachim.jasmin-dev@proton.me>
2026-06-25 15:51:07 +0200

    refactor: improved restaurant model for graphql

--- src/restaurant/restaurant.model.ts
+++ src/restaurant/restaurant.model.ts
@@ -2,24 +2,11 @@ import { Field, Int, ObjectType } from '@nestjs/graphql';
 
 @ObjectType()
 export class Restaurant {
-  @Field(() => Int)
-  id!: number;
-
-  @Field()
-  name!: string;
-
-  @Field()
-  line1!: string;
-
-  @Field({ nullable: true })
-  line2?: string;
-
-  @Field()
-  city!: string;
-
-  @Field()
-  postCode!: string;
-
-  @Field()
-  country!: string;
+  @Field(() => Int) id!: number;
+  @Field() name!: string;
+  @Field() line1!: string;
+  @Field({ nullable: true }) line2?: string;
+  @Field() city!: string;
+  @Field() postCode!: string;
+  @Field() country!: string;
 }

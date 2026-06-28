0db02c116e193fa8d153a6280fc513b6b7f6bdb3
Joachim Alexandre Jasmin <joachim.jasmin-dev@proton.me>
2026-06-28 12:44:53 +0200

    refactor: simplified diner model

--- src/diner/diner.model.ts
+++ src/diner/diner.model.ts
@@ -3,8 +3,8 @@ import { Field, Int, ObjectType } from '@nestjs/graphql';
 @ObjectType()
 export class Diner {
   @Field(() => Int) id!: number;
-  @Field(() => String) email!: string;
-  @Field(() => String) phone!: string;
-  @Field(() => String) firstName!: string;
-  @Field(() => String) lastName!: string;
+  @Field() email!: string;
+  @Field() phone!: string;
+  @Field() firstName!: string;
+  @Field() lastName!: string;
 }
